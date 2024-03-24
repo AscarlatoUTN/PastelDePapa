0b0834ef4eb3bbd557e863137c1b9363c32b10e5
AscarlatoUTN <ascarlato@frba.utn.edu.ar>
2024-03-24 20:44:15 -0300

    Modified: - notes (added new commands) - Library.hs (added Point-free style)

--- src/Library.hs
+++ src/Library.hs
@@ -4,13 +4,15 @@ import PdePreludat
 -- Does all math operations by default
 
 doble :: Number -> Number
-doble numero = numero + numero
-
-mitad :: Number -> Number
-mitad numero = numero / 2
+doble numero = numero * 2
 
 suma :: Number -> Number -> Number
 suma numero1 numero2 = numero1 + numero2
 
+-- Point-free style
+
+mitad :: Number -> Number
+mitad = (/2)
+
 resta :: Number -> Number -> Number
-resta numero1 numero2 = numero1 - numero2
+resta = (-)
