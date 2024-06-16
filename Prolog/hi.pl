ce8c74668c59e6ac5f869c98edd49a3c1c5c7582
AscarlatoUTN <ascarlato@frba.utn.edu.ar>
2024-06-16 16:37:18 -0300

    TP ayudaRasta

--- Prolog/hi.pl
+++
@@ -1,9 +0,0 @@
-% Definir relaciones de parentesco
-padre(juan, maria).
-padre(pablo, juan).
-padre(pablo, annette).
-
-% Regla para determinar si alguien es abuelo de alguien
-abuelo(X, Y) :-
-    padre(X, Z),  % X es padre de Z
-    padre(Z, Y).  % Z es padre de Y
